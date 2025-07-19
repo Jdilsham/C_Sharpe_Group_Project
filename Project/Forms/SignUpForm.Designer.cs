8c884b5d161ba25ca2c054f2900e5e005c9d296b
Jdilsham <>
2025-07-19 13:30:15 +0530

    add

--- Project/Forms/SignUpForm.Designer.cs
+++ Project/Forms/SignUpForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignUpForm));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.name_lbe = new System.Windows.Forms.Label();
@@ -36,8 +37,13 @@
             this.name_txt = new System.Windows.Forms.TextBox();
             this.email_txt = new System.Windows.Forms.TextBox();
             this.pass_txt = new System.Windows.Forms.TextBox();
-            this.term_check = new System.Windows.Forms.CheckBox();
             this.Signup_btn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Login_link = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +51,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(64, 87);
+            this.label1.Location = new System.Drawing.Point(64, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(214, 31);
             this.label1.TabIndex = 0;
@@ -65,7 +71,7 @@
             // 
             this.name_lbe.AutoSize = true;
             this.name_lbe.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.name_lbe.Location = new System.Drawing.Point(66, 172);
+            this.name_lbe.Location = new System.Drawing.Point(66, 117);
             this.name_lbe.Name = "name_lbe";
             this.name_lbe.Size = new System.Drawing.Size(56, 21);
             this.name_lbe.TabIndex = 2;
@@ -75,7 +81,7 @@
             // 
             this.email_lbe.AutoSize = true;
             this.email_lbe.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.email_lbe.Location = new System.Drawing.Point(66, 240);
+            this.email_lbe.Location = new System.Drawing.Point(66, 185);
             this.email_lbe.Name = "email_lbe";
             this.email_lbe.Size = new System.Drawing.Size(53, 21);
             this.email_lbe.TabIndex = 3;
@@ -85,7 +91,7 @@
             // 
             this.pass_lbe.AutoSize = true;
             this.pass_lbe.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pass_lbe.Location = new System.Drawing.Point(66, 311);
+            this.pass_lbe.Location = new System.Drawing.Point(66, 256);
             this.pass_lbe.Name = "pass_lbe";
             this.pass_lbe.Size = new System.Drawing.Size(82, 21);
             this.pass_lbe.TabIndex = 4;
@@ -97,7 +103,7 @@
             this.name_txt.BackColor = System.Drawing.SystemColors.Window;
             this.name_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.name_txt.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.name_txt.Location = new System.Drawing.Point(70, 196);
+            this.name_txt.Location = new System.Drawing.Point(70, 141);
             this.name_txt.Name = "name_txt";
             this.name_txt.Size = new System.Drawing.Size(322, 25);
             this.name_txt.TabIndex = 5;
@@ -109,10 +115,11 @@
             this.email_txt.BackColor = System.Drawing.SystemColors.Window;
             this.email_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.email_txt.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.email_txt.Location = new System.Drawing.Point(70, 264);
+            this.email_txt.Location = new System.Drawing.Point(70, 209);
             this.email_txt.Name = "email_txt";
             this.email_txt.Size = new System.Drawing.Size(322, 25);
             this.email_txt.TabIndex = 6;
+            this.email_txt.TextChanged += new System.EventHandler(this.email_txt_TextChanged);
             // 
             // pass_txt
             // 
@@ -120,23 +127,11 @@
             this.pass_txt.BackColor = System.Drawing.SystemColors.Window;
             this.pass_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pass_txt.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pass_txt.Location = new System.Drawing.Point(70, 335);
+            this.pass_txt.Location = new System.Drawing.Point(70, 280);
             this.pass_txt.Name = "pass_txt";
             this.pass_txt.Size = new System.Drawing.Size(322, 25);
             this.pass_txt.TabIndex = 7;
             this.pass_txt.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // term_check
-            // 
-            this.term_check.AccessibleName = "term_check";
-            this.term_check.AutoSize = true;
-            this.term_check.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.term_check.Location = new System.Drawing.Point(70, 383);
-            this.term_check.Name = "term_check";
-            this.term_check.Size = new System.Drawing.Size(168, 17);
-            this.term_check.TabIndex = 8;
-            this.term_check.Text = "I agree to the terms and policy";
-            this.term_check.UseVisualStyleBackColor = false;
             // 
             // Signup_btn
             // 
@@ -145,7 +140,7 @@
             this.Signup_btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Signup_btn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Signup_btn.ForeColor = System.Drawing.Color.White;
-            this.Signup_btn.Location = new System.Drawing.Point(70, 429);
+            this.Signup_btn.Location = new System.Drawing.Point(70, 374);
             this.Signup_btn.Name = "Signup_btn";
             this.Signup_btn.Size = new System.Drawing.Size(322, 33);
             this.Signup_btn.TabIndex = 10;
@@ -153,13 +148,78 @@
             this.Signup_btn.UseVisualStyleBackColor = false;
             this.Signup_btn.Click += new System.EventHandler(this.Signup_btn_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 574);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Have an account? ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(70, 326);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(168, 17);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "I agree to the terms and policy";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(70, 457);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(322, 1);
+            this.panel1.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(222, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Or";
+            // 
+            // Login_link
+            // 
+            this.Login_link.AccessibleName = "Login_link";
+            this.Login_link.AutoSize = true;
+            this.Login_link.Location = new System.Drawing.Point(255, 574);
+            this.Login_link.Name = "Login_link";
+            this.Login_link.Size = new System.Drawing.Size(33, 13);
+            this.Login_link.TabIndex = 18;
+            this.Login_link.TabStop = true;
+            this.Login_link.Text = "Login";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(70, 485);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 25);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Sign in with Google";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // SignUpForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Login_link);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.Signup_btn);
-            this.Controls.Add(this.term_check);
             this.Controls.Add(this.pass_txt);
             this.Controls.Add(this.email_txt);
             this.Controls.Add(this.name_txt);
@@ -188,7 +248,12 @@
         private System.Windows.Forms.TextBox name_txt;
         private System.Windows.Forms.TextBox email_txt;
         private System.Windows.Forms.TextBox pass_txt;
-        private System.Windows.Forms.CheckBox term_check;
         private System.Windows.Forms.Button Signup_btn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel Login_link;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Project/Forms/SignUpForm.cs
+++ Project/Forms/SignUpForm.cs
@@ -1,52 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace Project.Forms
 {
     public partial class SignUpForm : Form
     {
+        // Constructor
         public SignUpForm()
         {
             InitializeComponent();
+   
         }
 
+  
         private void SignUpForm_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+
+        private void name_txt_TextChanged(object sender, EventArgs e) { }
+
+        private void Signup_btn_Click(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void email_txt_TextChanged(object sender, EventArgs e) { }
+
+        private void term_check_CheckedChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void label6_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void name_txt_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Signup_btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-
     }
 }
